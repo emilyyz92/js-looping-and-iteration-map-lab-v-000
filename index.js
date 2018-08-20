211c4905ac08a47f9eacd1202d0eee7041a106d5
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-20 20:50:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file.
 function lowerCaseDrivers(array) {
-  array.map(function(string) {return string.toLowerCase()});
+  array.map(function(string) {
+    return string.toLowerCase()
+  }
+);
 }
